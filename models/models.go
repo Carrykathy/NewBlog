16ba7fe5e5bcf77107c26020a6ae05c5731ba38d
LL <nishuixunzhou@qq.com>
2025-07-10 15:29:35 +0800

    增加了回调函数

--- models/models.go
+++ models/models.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	"log"
+	"time"
 )
 
 var db *gorm.DB
@@ -52,8 +53,35 @@ func init() {
 	db.LogMode(true)
 	db.DB().SetMaxIdleConns(10)
 	db.DB().SetMaxOpenConns(100)
+
+	db.Callback().Create().Replace("gorm:update_time_stamp_for_create", updateTimeStampForCreateCallback)
+	db.Callback().Update().Replace("gorm:update_time_stamp_for_update", updateTimeStampForUpdateCallback)
 }
 
 func CloseDB() {
 	defer db.Close()
+}
+
+// 实现了创建时间戳和修改时间戳的回调函数
+func updateTimeStampForCreateCallback(scope *gorm.Scope) {
+	if !scope.HasError() {
+		nowTime := time.Now().Unix()
+		if createTimeField, ok := scope.FieldByName("CreatedOn"); ok {
+			if createTimeField.IsBlank {
+				createTimeField.Set(nowTime)
+			}
+		}
+
+		if modifyTimeField, ok := scope.FieldByName("ModifiedOn"); ok {
+			if modifyTimeField.IsBlank {
+				modifyTimeField.Set(nowTime)
+			}
+		}
+	}
+}
+
+func updateTimeStampForUpdateCallback(scope *gorm.Scope) {
+	if _, ok := scope.Get("gorm:update_column"); !ok {
+		scope.SetColumn("ModifiedOn", time.Now().Unix())
+	}
 }
